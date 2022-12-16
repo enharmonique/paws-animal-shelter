971cad08bc39769d163fad9bc313368dc4ffe0a8
Zotta317 <victor.zotta@gmail.com>
2022-12-16 21:37:57 +0200

    Login/Sign UP UI -voi mai adauga ceva features peste

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
-import {MatButtonModule} from '@angular/material/button';
+// import {MatButtonModule} from '@angular/material/button';
 
 import {AppComponent} from './app.component';
 import {UserComponent} from './components/user/user.component';
@@ -11,11 +11,17 @@ import {HeaderComponent} from './components/header/header.component';
 import {AnimalListComponent} from './components/animal-list/animal-list.component';
 import {HomePageComponent} from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from "@angular/forms";
+import { SignupComponent } from './components/signup/signup.component';
 
 const appRoutes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path:'signup',
+    component: SignupComponent,
   },
 //   // {
 //   //   path: 'register',
@@ -44,13 +50,15 @@ const appRoutes: Routes = [
     HeaderComponent,
     AnimalListComponent,
     HomePageComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
-    MatButtonModule
+    FormsModule
+    // MatButtonModule
   ],
   exports: [
     RouterModule
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  // loginObj: any = {
+  //   userName: '',
+  //   password:''
+  // };
+  type:string="password";
+  isText: boolean = false;
+  eyeICon: string ="fa-eye-slash"
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
+  // onLogin() {
+  //   if(this.loginObj.userName == 'user123' && this.loginObj.password =='user@123') {
+  //     localStorage.setItem('role','user');
+  //     this.router.navigateByUrl('http://localhost:4200/');
+  //   } else if (this.loginObj.userName == 'admin' && this.loginObj.password =='admin@123') {
+  //     localStorage.setItem('role','admin');
+  //     this.router.navigateByUrl('http://localhost:4200/');
+  //   }
+  // }
+  hideShowPass(){
+      this.isText = !this.isText;
+      this.isText ? this.eyeICon = "fa-eye": this.eyeICon ="fa-eye-slash";
+      this.isText ? this.type="text" : this.type = "password"
+  }
 }
