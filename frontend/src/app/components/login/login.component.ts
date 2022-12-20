a96b9be7a136e608308b68ee9b87dcc15a5dc6c3
Zotta317 <victor.zotta@gmail.com>
2022-12-20 20:05:47 +0200

    User profile in progress

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -31,7 +31,7 @@ export class LoginComponent implements OnInit {
     //True if all the fields are filled
     let routerLink;
     if (this.submitted) {
-      routerLink = "/signup";
+      routerLink = "/events";
     }
 
   }
--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -29,7 +29,7 @@ export class SignupComponent implements OnInit {
     //True if all the fields are filled
     let routerLink;
     if (this.submitted) {
-      routerLink = "/signup";
+      routerLink = "/events";
     }
 
   }
